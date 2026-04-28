92f86c77607a51037f78dbf222a213b42976bf8b
= <catarina0.silvaaa0@gmail.com>
2026-04-28 22:26:33 +0100

    Add .gitignore and remove vendor from tracking

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '7a5bc43fcc76c38d';
+  public const string VERSIONS_HASH = 'fafb4f9781cec88f';
 
 }
